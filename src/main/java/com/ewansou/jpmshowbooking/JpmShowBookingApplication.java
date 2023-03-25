bdb8d0c586294b148b6f8c3f63e05acc7b97bebb
ewansou <ewansou@hotmail.com>
2023-03-25 10:19:37 +0800

    initial commit

---
+++ src/main/java/com/ewansou/jpmshowbooking/JpmShowBookingApplication.java
@@ -0,0 +1,13 @@
+package com.ewansou.jpmshowbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpmShowBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpmShowBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ewansou/jpmshowbooking/JpmShowBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewansou.jpmshowbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpmShowBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
